d77e737caffa3293aff4a797d62b040220d3700f
tothaiduong507 <74888409+tothaiduong507@users.noreply.github.com>
2024-03-15 23:19:14 +0700

    ex6.3 commit

---
+++ homework/TriangleHeightN/src/TriangleHeightN.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class TriangleHeightN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the height of the triangle: ");
+        int height = scanner.nextInt();
+        displayTriangle(height);
+        scanner.close();
+    }
+
+    public static void displayTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
